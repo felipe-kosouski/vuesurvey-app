14107de38d970c9f780c6319ee479a117954b050
Felipe Kosouski <felipe.kosouski@gmail.com>
2019-05-06 22:36:08 -0300

    Layout Finished

---
+++ src/components/layout/Header.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <v-toolbar color="primary white--text">
+            <v-toolbar-title>Survey App</v-toolbar-title>
+        </v-toolbar>
+    </div>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/surveys/New.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <v-btn fab dark color="primary">
+            <v-icon dark>add</v-icon>
+        </v-btn>
+    </div>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style scoped>
+
+</style>
